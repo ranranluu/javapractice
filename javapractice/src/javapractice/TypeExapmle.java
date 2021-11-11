a8b464a48ac67112b86cc9eb77359171448d6723
ranranluu <ranranluutennis@gmail.com>
2021-11-11 16:17:23 +0900

    firstcommit

---
+++ javapractice/src/javapractice/TypeExapmle.java
@@ -0,0 +1,21 @@
+package javapractice;
+
+public class TypeExapmle {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		double d;
+		d = 9.9;
+		char c;
+		c = 'あ';
+		byte b;
+		b = 81;
+		boolean bool;
+		bool = true;
+		System.out.println("dの値は" + d);
+		System.out.println("cの値は" + c);
+		System.out.println("bの値は" + b);
+		System.out.println("boolの値は" + bool);
+	}
+
+}
