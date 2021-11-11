3143a0c8ae766d939c3cb1f3e59ecd949d5137e6
ranranluu <ranranluutennis@gmail.com>
2021-11-11 16:10:09 +0900

    firstcommit

---
+++ javapractice/src/javapractice/VariableExample2.java
@@ -0,0 +1,14 @@
+package javapractice;
+
+public class VariableExample2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		i = 5;
+		System.out.println(i);
+		i = 10;
+		System.out.println(i);
+	}
+
+}
