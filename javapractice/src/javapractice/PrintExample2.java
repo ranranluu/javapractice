f4c26b0f5e4350c37a281662ce72f9cbd62a107d
ranranluu <ranranluutennis@gmail.com>
2021-11-11 16:01:05 +0900

    firstcommit

---
+++ javapractice/src/javapractice/PrintExample2.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class PrintExample2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.print("こんにちは");
+		System.out.print("今日も良い天気です");
+	}
+
+}
